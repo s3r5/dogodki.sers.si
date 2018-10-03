d22216ffc392b37b48511d1e0cc74f36cdb6c160
Miha Frangež <miha.frangez@gmail.com>
2018-10-03 22:14:30 +0200

    Let there be Django!

---
+++ dogodki_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DogodkiConfig(AppConfig):
+	name = 'dogodki'
---
+++ dogodki_core/urls.py
@@ -0,0 +1,11 @@
+"""dogodki_django URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+	https://docs.djangoproject.com/en/2.1/topics/http/urls/
+"""
+from django.contrib import admin
+from django.urls import path
+
+urlpatterns = [
+	path('admin/', admin.site.urls),
+]
