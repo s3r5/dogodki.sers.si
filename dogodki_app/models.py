f19ac18892c20cd27368ea6110694ed30aacd386
Miha Frangež <miha.frangez@gmail.com>
2019-01-06 01:24:48 +0100

    Polje za oddelek
    (Pri združevanju vej se je ta del kode nekje izgubil)

--- dogodki_app/models.py
+++ dogodki_app/models.py
@@ -7,6 +7,7 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class User(AbstractUser):
+	oddelek = models.CharField(max_length=5, blank=True, null=True)
 	
 	def __str__(self):
 		return self.first_name + " " + self.last_name
