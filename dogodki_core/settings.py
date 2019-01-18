be39b2c39c0f301ee69e1920671bc75e9e7a8227
Miha Frangež <miha.frangez@gmail.com>
2019-01-18 11:34:44 +0100

    Popravljena asocialcija z obstoječimi uporabniki

--- dogodki_core/settings.py
+++ dogodki_core/settings.py
@@ -154,11 +154,11 @@ SOCIAL_AUTH_PIPELINE = (
 	'social_core.pipeline.social_auth.social_uid',
 	'social_core.pipeline.social_auth.social_user',
 	'social_core.pipeline.user.get_username',
+	'social_core.pipeline.social_auth.associate_by_email',
 	'social_core.pipeline.user.create_user',
 	'social_core.pipeline.social_auth.associate_user',
 	'social_core.pipeline.social_auth.load_extra_data',
 	'social_core.pipeline.user.user_details',
-	'social_core.pipeline.social_auth.associate_by_email',
 )
 
 SOCIAL_AUTH_MICROSOFT_GRAPH_KEY = env("MICROSOFT_AUTH_CLIENT_ID")
